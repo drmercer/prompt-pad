94d3379481b91850ab9e6ad38681b250cdf97fa7
Dan Mercer <danielmercer1@gmail.com>
2025-09-20 20:55:53 -0600

    Stub panel in Lucid extension

--- lucid-code-cards/editorextensions/code-cards/src/extension.ts
+++ lucid-code-cards/editorextensions/code-cards/src/extension.ts
@@ -1,9 +1,22 @@
-import {EditorClient, Menu, MenuType, Viewport} from 'lucid-extension-sdk';
+import {EditorClient, Menu, MenuType, Panel, PanelLocation, Viewport} from 'lucid-extension-sdk';
 import {log} from './logger';
 
 const client = new EditorClient();
 const menu = new Menu(client);
 const viewport = new Viewport(client);
+
+class MyPanel extends Panel {
+  constructor(client: EditorClient) {
+    super(client, {
+      iconUrl: 'http://danmercer.net/favicon.svg',
+      title: 'Code Cards',
+      location: PanelLocation.RightDock,
+      url: 'https://danmercer.net',
+    });
+  }
+}
+
+new MyPanel(client);
 
 viewport.hookSelection((selection) => {
   log('Selection changed', selection);
