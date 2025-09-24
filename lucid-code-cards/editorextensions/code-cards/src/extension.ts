d2707a3b55613353da86d4a832b364f160dd077e
Dan Mercer <danielmercer1@gmail.com>
2025-09-23 21:19:46 -0600

    More extension tinkering

--- lucid-code-cards/editorextensions/code-cards/src/extension.ts
+++ lucid-code-cards/editorextensions/code-cards/src/extension.ts
@@ -1,14 +1,13 @@
-import {EditorClient, Menu, MenuType, Panel, PanelLocation, Viewport} from 'lucid-extension-sdk';
+import {BlockProxy, EditorClient, Menu, Panel, PanelLocation, Viewport} from 'lucid-extension-sdk';
 import {log} from './logger';
 
 const client = new EditorClient();
-const menu = new Menu(client);
 const viewport = new Viewport(client);
 
 class MyPanel extends Panel {
   constructor(client: EditorClient) {
     super(client, {
-      iconUrl: 'http://danmercer.net/favicon.svg',
+      iconUrl: 'https://danmercer.net/favicon.svg',
       title: 'Code Cards',
       location: PanelLocation.RightDock,
       url: 'https://danmercer.net',
@@ -16,14 +15,25 @@ class MyPanel extends Panel {
   }
 }
 
-new MyPanel(client);
+client.registerAction('open-code-cards', () => {
+  log('Action: Open Code Cards');
+  panel.show();
+});
+
+const menu = new Menu(client);
+menu.addContextMenuItem({
+  label: 'Open Code Cards',
+  action: 'open-code-cards',
+});
+
+const panel = new MyPanel(client);
 
 viewport.hookSelection((selection) => {
-  log('Selection changed', selection);
+  if (selection.length == 1 && selection[0] instanceof BlockProxy) {
+    const block = selection[0] as BlockProxy;
+    log('Selected block ID:', block.id, block.getClassName());
+    panel.show();
+  } else {
+    panel.hide();
+  }
 })
-
-menu.addMenuItem({
-    label: 'Show import modal',
-    action: 'show-import-modal',
-    menuType: MenuType.Main,
-});
